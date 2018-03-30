adbd33d1b284bb01d6b263a697c18a246b28b5f4
Ma <alma@paypal.com>
2018-03-30 16:27:24 +0800

    first

---
+++ WuKong.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+from module import a
+
+
+@app.route('/')
+def index():
+    # dgfsfdgs
+    # todo
+    # a.xxx()
+    # a.xxx()
+    # execute module code ...
+    # result = ...
+    return render_template("index.html", result='123', date='2018-03-30')
+
+@app.route('/integration')
+def integration():
+    return render_template('integration/main.html')
+
+
+@app.route('/shopping_cart')
+def shopping_cart():
+    return render_template('shopping_cart/main.html')
+
+@app.route('/faq')
+def faq():
+    return render_template('faq/main.html')
+
+
+if __name__ == '__main__':
+    app.run()
