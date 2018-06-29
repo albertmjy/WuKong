ba0cb6dad7d4ab3f6507bc3c33ed3d89c6cc8bb3
David/Adam Dai <wdai1@paypalcorp.com>
2018-06-29 10:40:46 +0800

    configure DB layer.

--- WuKong.py
+++ WuKong.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, request
-from database import *
-from models import *
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
 
 
 @app.route('/')
@@ -15,6 +18,7 @@ def index():
     # result = ...
     return render_template("index.html", result='123', date='2018-03-30')
 
+
 @app.route('/integration')
 def integration():
     return render_template('integration/main.html')
@@ -24,13 +28,16 @@ def integration():
 def shopping_cart():
     return render_template('shopping_cart/main.html')
 
+
 @app.route('/faq')
 def faq():
     return render_template('faq/main.html')
 
+
 @app.route('/step')
 def step():
     return render_template('integration/step.html')
+
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
